4cb190c3689267be2a4102bf2e9f2dcf71e979e9
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-12 18:05:47 +0000

    Add media list class

--- list/views.py
+++ list/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class MediaList(generic.ListView):
+    model = Media
+    queryset = Media.objects.filter(status=0).order_by('title')
+    template_name = 'index.html'
+    paginate_by = 10
