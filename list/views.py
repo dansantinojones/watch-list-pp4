4c26098040d34ada80b3786a0fca8d9890e7adf3
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-13 10:12:38 +0000

    Add media detail class

--- list/views.py
+++ list/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Media
 
 
@@ -8,3 +8,24 @@ class MediaList(generic.ListView):
     queryset = Media.objects.filter(status=0).order_by('title')
     template_name = 'index.html'
     paginate_by = 10
+
+
+class MediaDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Media.objects.filter(status=0)
+        media = get_object_or_404(queryset, slug=slug)
+        comments = media.comments.filter(approved=True).order_by('created_on')
+        recommended = False
+        if media.recommended.filter(id=self.request.user.id).exists():
+            recommended = True
+
+        return render(
+            request,
+            "media_detail.html",
+            {
+                "media": media,
+                "comments": comments,
+                "recommended": recommended
+            },
+        )
