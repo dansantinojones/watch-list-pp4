af48bd667f3ad3b9edaeeac2fe9e129eea117710
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-13 10:11:45 +0000

    Add media detail path

--- list/urls.py
+++ list/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.MediaList.as_view(), name='home')
+    path('', views.MediaList.as_view(), name='home'),
+    path('<slug:slug>/', views.MediaDetail.as_view(), name='media_detail'),
 ]
