e42d1b0ac2d44dc59f8c20f6eb453ff919bacbd9
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-16 10:55:19 +0000

    Add crispy forms to installed apps

--- watchlist/settings.py
+++ watchlist/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'list',
 ]
 
@@ -58,6 +59,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
