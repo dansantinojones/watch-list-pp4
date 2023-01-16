7827af01dbad65dc397c052aa41ceb2468e4205b
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-16 10:57:22 +0000

    Add post data from form

--- list/views.py
+++ list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Media
+from .forms import CommentForm
 
 
 class MediaList(generic.ListView):
@@ -26,6 +27,41 @@ class MediaDetail(View):
             {
                 "media": media,
                 "comments": comments,
-                "recommended": recommended
+                "commented": False,
+                "recommended": recommended,
+                "comment_form": CommentForm()
+            },
+        )
+
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Media.objects.filter(status=0)
+        media = get_object_or_404(queryset, slug=slug)
+        comments = media.comments.filter(approved=True).order_by('created_on')
+        recommended = False
+        if media.recommended.filter(id=self.request.user.id).exists():
+            recommended = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.media = media
+            comment.save()
+
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "media_detail.html",
+            {
+                "media": media,
+                "comments": comments,
+                "commented": True,
+                "recommended": recommended,
+                "comment_form": CommentForm()
             },
         )
