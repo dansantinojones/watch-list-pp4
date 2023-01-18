0da7052962d511909d03b598c6b0b00c3f31ecdd
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-18 19:30:30 +0000

    Create add media path

--- list/urls.py
+++ list/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.MediaList.as_view(), name='home'),
-    path('<slug:slug>/', views.MediaDetail.as_view(), name='media_detail'),
+    path('add_media/', views.AddMedia.as_view(), name='add_media'),
     path('recommended/<slug:slug>', views.MediaRecommended.as_view(), name='media_recommended'),
+    path('<slug:slug>/', views.MediaDetail.as_view(), name='media_detail'),
 ]
