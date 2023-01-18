b6fd63ecc348adfc951a2d2e462e46c17f1b319c
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-18 19:27:59 +0000

    Create form to add media

--- list/forms.py
+++ list/forms.py
@@ -1,8 +1,21 @@
 from .models import Comment
 from django import forms
+from cloudinary.models import CloudinaryField
 
 
 class CommentForm(forms.ModelForm):
     class Meta: 
         model = Comment
         fields = ('body',)
+
+
+class AddMedia(forms.Form):
+    title = forms.CharField(max_length=50)
+    description = forms.CharField(widget=forms.Textarea)
+    platform = forms.CharField(max_length=20)
+    genre = forms.CharField(max_length=20)
+    type = forms.CharField(max_length=20)
+    media_image = CloudinaryField('image', default='placeholder')
+
+    def send_recommendation(self):
+        pass
