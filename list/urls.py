2fa2ba60d5a0ea2a3463ac95d85ce92e6832da90
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-22 12:35:23 +0000

    Add delete media path

--- list/urls.py
+++ list/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.MediaList.as_view(), name='home'),
     path('add_media/', views.AddMedia.as_view(), name='add_media'),
     path('list/edit_media/<slug:slug>', views.EditMedia.as_view(), name='edit_media'),
+    path('list/delete_media/<slug:slug>', views.DeleteMedia.as_view(), name='delete_media'),
     path('recommended/<slug:slug>', views.MediaRecommended.as_view(), name='media_recommended'),
     path('<slug:slug>/', views.MediaDetail.as_view(), name='media_detail'),
 ]
