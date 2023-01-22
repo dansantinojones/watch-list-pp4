6e6851137e0676b4e168aae23d3e65cf0213b5bd
dansantinojones <dansantinojones@hotmail.co.uk>
2023-01-22 10:52:55 +0000

    Add edit media path

--- list/urls.py
+++ list/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.MediaList.as_view(), name='home'),
     path('add_media/', views.AddMedia.as_view(), name='add_media'),
+    path('list/edit_media/<slug:slug>', views.EditMedia.as_view(), name='edit_media'),
     path('recommended/<slug:slug>', views.MediaRecommended.as_view(), name='media_recommended'),
     path('<slug:slug>/', views.MediaDetail.as_view(), name='media_detail'),
 ]
